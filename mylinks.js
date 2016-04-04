9ebfbf74e10f5cb3ad9dded8a5b36bcd19ef389e
Anjib Mulepati <mulepatianjib@johndeere.com>
2016-04-04 12:12:19 -0500

    Helpful links

---
+++ mylinks.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function() {
+  jQuery('.tabs .tab-links a').on('click', function(e)  {
+    var currentAttrValue = jQuery(this).attr('href');
+    jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+    jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+    e.preventDefault();
+  });
+});
